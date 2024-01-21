bf633c3d0102dfef07f002d0df19a07ce652b189
ratna <devopsevd@users.noreply.github.com>
2024-01-20 23:07:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,22 @@
 
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+from st_aggrid import AgGrid
 
 # Create a connection object.
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+# df = conn.read()
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.core} has a :{row.short}:")
+# for row in df.itertuples():
+#     st.write(f"{row.core} has a :{row.short}:")
+
+df = conn.read(
+    worksheet="summary",
+    ttl="10m",
+    usecols=[0, 1,2,3],
+    nrows=15,
+)
+
+AgGrid(df)
